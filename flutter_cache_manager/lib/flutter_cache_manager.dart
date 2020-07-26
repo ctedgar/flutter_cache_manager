90b487fe0c6c772665ddd6f6534d11bcd9ca6bd6
Cris Edgar <me@crisedgar.com>
2020-07-26 15:31:26 +0100

    expose internal cache structures

--- flutter_cache_manager/lib/flutter_cache_manager.dart
+++ flutter_cache_manager/lib/flutter_cache_manager.dart
@@ -1,5 +1,9 @@
 export 'src/cache_manager.dart';
+export 'src/cache_store.dart';
 export 'src/compat/file_fetcher.dart';
 export 'src/result/result.dart';
+export 'src/storage/cache_info_repository.dart';
+export 'src/storage/cache_object.dart';
+export 'src/storage/cache_object_provider.dart';
 export 'src/web/file_service.dart';
 export 'src/web/web_helper.dart' show HttpExceptionWithStatus;
